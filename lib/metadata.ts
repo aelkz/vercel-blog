a4b6ed603d0776d5c0a668ce0664a469034bfa12
raphael alex <14264577+aelkz@users.noreply.github.com>
2025-12-02 18:04:45 -0300

    wip: social sharing buttons
    Signed-off-by: raphael alex <14264577+aelkz@users.noreply.github.com>

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -3,7 +3,7 @@ import { AuthorType, SiteMetaData } from "@/types";
 import { socialProfiles } from "./social-data";
 
 export const BASE_URL =
-  `https://${process.env.VERCEL_URL}` ||
+  (process.env.VERCEL_URL && `https://${process.env.VERCEL_URL}`) ||
   process.env.NEXT_PUBLIC_BASE_URL ||
   `http://localhost:${process.env.PORT || 3000}`;
 
